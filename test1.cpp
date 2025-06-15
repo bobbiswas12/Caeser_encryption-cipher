a72c7bb0c039f46837a8dd9b68874b0f9b4df176
bobbiswas12 <148140393+bobbiswas12@users.noreply.github.com>
2025-06-15 22:25:02 +0530

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 int main(){
   string original_text;
+  int shift;
+  cin>>"Enter the shift (int)"<<endl;
   cout<<"Enter the original string"<<endl;
   cin>>original_text;
   cout<<"The encrypted text is "<<endl;
-  cout<<caeser_cipher_encryption(original_text)<<endl;
+  cout<<caeser_cipher_encryption(original_text,shift)<<endl;
   return 0;
 }
