b7731e447e8550074823e9af2272cfd423cf957d
bobbiswas12 <148140393+bobbiswas12@users.noreply.github.com>
2025-06-16 09:50:21 +0530

    Added functionality to include sentences as input

--- test1.cpp
+++ test1.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <stdio.h>
 #include "caeser_cipher_encryption.cpp"
+
 using namespace std;
 
 int main(){
   string original_text;
+  cout<<"Enter the original text"<<endl;
+  getline(cin, original_text);
   int shift;
-  cout<<"Enter the shift (int)"<<endl;
+  cout<<"Enter the shift"<<endl;
   cin>>shift;
-  cout<<"Enter the original string"<<endl;
-  cin>>original_text;
-  cout<<"The encrypted text is "<<endl;
   cout<<caeser_cipher_encryption(original_text,shift)<<endl;
   return 0;
 }
